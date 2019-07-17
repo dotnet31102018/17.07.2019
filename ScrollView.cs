47c13ccf6c689b42cd2f44e672cc747dc78e4a1b
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-07-17 20:20:02 +0300

    Create ScrollView.cs

---
+++ ScrollView.cs
@@ -0,0 +1,11 @@
+        <ScrollViewer
+            Grid.Row="1" Grid.Column="4"
+            Grid.RowSpan="6" Height="400">
+            <StackPanel >
+                <Image Source="Resources/pic.jpg" />
+                <Image Source="Resources/pic.jpg" />
+                <Image Source="Resources/pic.jpg" />
+                <Image Source="Resources/pic.jpg" />
+            </StackPanel>
+            
+        </ScrollViewer>
